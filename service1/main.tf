2fae338a72ea12092414b90c6c003987585a84c4
joz-incubator <gudauskas.juozas@gmail.com>
2025-06-30 22:43:02 +0200

    Update main.tf

--- service1/main.tf
+++ service1/main.tf
@@ -12,8 +12,8 @@ resource "google_compute_subnetwork" "default" {
   ip_cidr_range = "10.0.1.0/24"
   region        = "europe-west6"
   network       = google_compute_network.vpc_network.id
-  allow_subnet_cidr_routes_overlap = false
+  private_ip_google_access   = true
+  private_ipv6_google_access = false
   enable_flow_logs                 = false
-  private_ip_google_access         = true
   stack_type                       = "IPV4_ONLY"
 }
