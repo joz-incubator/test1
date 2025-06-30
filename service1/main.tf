f02dc7526daca22d2bf806d3f34c5e21ce603a8c
joz-incubator <gudauskas.juozas@gmail.com>
2025-06-30 22:14:20 +0200

    Update main.tf

--- service1/main.tf
+++ service1/main.tf
@@ -1,14 +1,19 @@
 resource "google_compute_network" "vpc_network" {
-  name                    = "test-vpc1"
+  name                    = "service-vpc-test1"
   project                 = "he-prod-itinfra-incubator"
   auto_create_subnetworks = false
   mtu                     = 1460
+  enable_Ula_Internal_Ipv6 = false
 }
 
 resource "google_compute_subnetwork" "default" {
-  name          = "my-custom-subnet"
-  project                 = "he-prod-itinfra-incubator"
+  name          = "service-vpc-test1"
+  project       = "he-prod-itinfra-incubator"
   ip_cidr_range = "10.0.1.0/24"
   region        = "europe-west6"
   network       = google_compute_network.vpc_network.id
+  allow_Subnet_Cidr_Routes_Overlap = false
+  enable_Flow_Logs                 = false
+  private_Ip_Google_Access         = true
+  stack_Type                       = "IPV4_ONLY"
 }
