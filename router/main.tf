ebed7ab4bb412ada101be93f4ac362ede0960fa7
joz-incubator <gudauskas.juozas@gmail.com>
2025-06-30 23:38:16 +0200

    Update main.tf

--- router/main.tf
+++ router/main.tf
@@ -3,13 +3,4 @@ resource "google_compute_router" "router" {
   project                       = "he-prod-itinfra-incubator"
   network                       = "service-vpc-test1"
   region                        = "europe-west6"
-
-  dynamic "bgp" {
-    for_each = var.bgp != null ? [var.bgp] : []
-    content {
-      advertise_mode     = "DEFAULT"
-      advertise_groups = ["ALL_SUBNETS"]
-    }
-  }
-
 }
