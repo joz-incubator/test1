0f6aba8ed58d10dc6704ec6048af47da383d1d31
joz-incubator <gudauskas.juozas@gmail.com>
2025-06-30 21:35:25 +0200

    Create main.tf

---
+++ vpc2/main.tf
@@ -0,0 +1,12 @@
+resource "google_compute_network" "vpc_network" {
+  name                    = "test-vpc1"
+  auto_create_subnetworks = false
+  mtu                     = 1460
+}
+
+resource "google_compute_subnetwork" "default" {
+  name          = "my-custom-subnet"
+  ip_cidr_range = "10.0.1.0/24"
+  region        = "europe-west6"
+  network       = google_compute_network.vpc_network.id
+}
