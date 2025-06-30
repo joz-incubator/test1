d2b14427a1dd97189692cda470108d356ae54554
joz-incubator <gudauskas.juozas@gmail.com>
2025-07-01 00:09:40 +0200

    Update nat.tf

--- nat/nat.tf
+++ nat/nat.tf
@@ -6,7 +6,7 @@ resource "google_compute_project_default_network_tier" "project-tier" {
 resource "google_compute_router_nat" "nat" {
   name                                = "service-nat-test1"
   project                             = "he-prod-itinfra-incubator"
-  router                              = "service-caloud-router"
+  router                              = "service-cloud-router"
   region                              = "europe-west6"
   nat_ip_allocate_option              = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat  = "ALL_SUBNETWORKS_ALL_IP_RANGES"
