0438a52e94c7ec61cd66b28b2c759b3836799e6a
joz-incubator <gudauskas.juozas@gmail.com>
2025-06-30 23:28:06 +0200

    Update main.tf

--- router/main.tf
+++ router/main.tf
@@ -1,4 +1,4 @@
-esource "google_compute_router" "router" {
+resource "google_compute_router" "router" {
   name                          = "service-cloud-router"
   project                       = "he-prod-itinfra-incubator"
   network                       = "service-vpc-test1"
