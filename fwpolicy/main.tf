f69b7f437d61b54169d327f36eaebb2a12d5c6b3
joz-incubator <gudauskas.juozas@gmail.com>
2025-07-01 12:37:20 +0200

    Update main.tf

--- fwpolicy/main.tf
+++ fwpolicy/main.tf
@@ -1,1 +1,22 @@
-TBD
+provider "google" {
+  project = "he-prod-itinfra-incubator"
+  region  = "europe-west6"
+}
+
+resource "google_compute_firewall" "default" {
+  name    = "allow-internal"
+  network = "service-vpc-test1"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+
+  allow {
+    protocol = "icmp"
+  }
+
+  source_ranges = ["10.0.0.0/16"]
+  direction     = "EGRESS"
+  priority      = 899
+}
