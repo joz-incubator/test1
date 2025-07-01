15e3ed9897dd02c65c69f8fedcf338ec98ae6147
joz-incubator <gudauskas.juozas@gmail.com>
2025-07-01 20:45:09 +0200

    Create main.tf

---
+++ service1/main.tf
@@ -0,0 +1,82 @@
+provider "google" {
+  project = "he-prod-itinfra-incubator"
+  region  = var.region
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "network-${var.customer}"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name          = "subnet-${var.customer}"
+  ip_cidr_range = var.cidr
+  region  = var.region
+  network       = google_compute_network.vpc_network.id
+}
+
+resource "google_compute_firewall" "egress_https" {
+  name    = "egress-${var.customer}"
+  network = google_compute_network.vpc_network.name
+  priority = 899
+  direction = "EGRESS"
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+
+  destination_ranges = slice(var.fwdestnets, 0, var.fwdestcount)
+}
+
+resource "google_compute_firewall" "ingress_ssh" {
+  name    = "ingress-${var.customer}"
+  network = google_compute_network.vpc_network.name
+  priority = 900
+  direction = "INGRESS"
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["35.235.240.0/20"]
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "vm-${var.customer}"
+  zone         = var.zone
+  depends_on   = [google_compute_subnetwork.subnet]
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "projects/ubuntu-os-cloud/global/images/ubuntu-minimal-2504-plucky-amd64-v20250624"
+      size  = 10
+      type  = "pd-balanced"
+      }
+    }
+
+    shielded_instance_config {
+      enable_integrity_monitoring = true
+      enable_secure_boot          = true
+      enable_vtpm                 = true
+  }
+
+  network_interface {
+    network    = google_compute_network.vpc_network.id
+    subnetwork = google_compute_subnetwork.subnet.id
+  }
+}
+
+resource "google_compute_router" "router" {
+  name    = "router-${var.customer}"
+  network = google_compute_network.vpc_network.name
+  region  = var.region
+}
+
+resource "google_compute_router_nat" "nat" {
+  name                               = "natgw-${var.customer}"
+  router                             = google_compute_router.router.name
+  region                             = var.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
