af1c9f7bc641617ff6455a336ed6fe4d64e38ab7
joz-incubator <gudauskas.juozas@gmail.com>
2025-07-01 07:57:22 +0200

    Delete service1/router.tf

--- service1/router.tf
+++
@@ -1,6 +0,0 @@
-resource "google_compute_router" "router" {
-  name                          = "service-cloud-router"
-  project                       = "he-prod-itinfra-incubator"
-  network                       = "service-vpc-test1"
-  region                        = "europe-west6"
-}
