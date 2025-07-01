1be9af9d8d06cd3313fe6e636f70455b0f6107be
joz-incubator <gudauskas.juozas@gmail.com>
2025-07-01 12:58:51 +0200

    Update main.tf

--- fwpolicy/main.tf
+++ fwpolicy/main.tf
@@ -3,20 +3,8 @@ provider "google" {
   region  = "europe-west6"
 }
 
-resource "google_compute_firewall" "default" {
-  name    = "allow-internal"
-  network = "service-vpc-test1"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["443"]
-  }
-
-  allow {
-    protocol = "icmp"
-  }
-
-  source_ranges = ["10.0.0.0/16"]
-  direction     = "EGRESS"
-  priority      = 899
+resource "google_compute_firewall_policy" "default" {
+  parent      = "organizations/30668948301"
+  short_name  = "fw-policy"
+  description = "Resource created for Terraform acceptance testing"
 }
