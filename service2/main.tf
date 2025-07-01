120501bbcdc155fa57f5c7e1e416f6ceb69ea3fb
joz-incubator <gudauskas.juozas@gmail.com>
2025-07-01 12:00:33 +0200

    Update main.tf

--- service2/main.tf
+++ service2/main.tf
@@ -33,4 +33,10 @@ resource "google_compute_instance" "compute_vm" {
     }
     mode = "READ_WRITE"
   }
+
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot          = true
+    enable_vtpm                 = true
+  }
 }
