acda2cd084daf6dadb9a5d98ea5acf12685e5cbe
joz-incubator <gudauskas.juozas@gmail.com>
2025-07-01 18:27:41 +0200

    Update main.tf

--- fwpolicy/main.tf
+++ fwpolicy/main.tf
@@ -8,12 +8,11 @@ resource "google_compute_firewall" "rules" {
   name        = "my-firewall-rule"
   network     = "service-vpc-test1"
   description = "Creates firewall rule targeting tagged instances"
-
+  direction   = "EGRESS"
+  destination_ranges = ["0.0.0.0/0"]
+  priority = 899
   allow {
     protocol = "tcp"
     ports    = ["443"]
   }
-
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["web"]
 }
