2c7ff2cd09f9c1f9dbe49d4ea5d4f8485ad0e503
joz-incubator <gudauskas.juozas@gmail.com>
2025-07-01 20:45:41 +0200

    Create variables.tf

---
+++ service2/variables.tf
@@ -0,0 +1,23 @@
+variable "region" {
+  default = "europe-west6"
+}
+
+variable "zone" {
+  default = "europe-west6-c"
+}
+
+variable "customer" {
+  default = "whatever3"
+}
+
+variable "cidr" {
+  default = "10.0.0.0/24"
+}
+
+variable "fwdestnets" {
+  default = ["0.0.0.0/0"]
+}
+
+variable "fwdestcount" {
+  default = 1
+}
